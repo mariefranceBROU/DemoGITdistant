37dffeffac3eab109705492d3a133dd52682efeb
mariefranceBROU <bmariefrance2904@gmail.com>
2018-05-14 19:50:41 +0000

    création de deux classes

---
+++ src/demo/git/GitPrincipal.java
@@ -0,0 +1,13 @@
+package demo.git;
+
+public class GitPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" ma premire demo git");
+		System.out.println(" ma premire demo git");
+		System.out.println(" ma premire demo git");
+	}
+
+}
